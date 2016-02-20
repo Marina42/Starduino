ef50dbae2b4a47d82d0881a6fc5799c45674156e
mariwi <marina.sauca@gmail.com>
2016-02-20 10:14:06 +0100

    stuff done

--- Python/filemonitor.py
+++ Python/filemonitor.py
@@ -9,13 +9,19 @@ app = QtGui.QApplication(sys.argv)
 
 print('wirks')
 
+filepath = '/Users/marina/Library/Application Support/Stellarium/output.txt'
+
 @QtCore.pyqtSlot(str)
 def file_changed(path):
+    coordFile = open(filepath, 'r')
+    print file.readline(coordFile)
     ser.write(str('hello')) # Convert the decimal number to ASCII then send it to the Arduino
     print ser.readline() # Read the newest output from the Arduino
+    coordFile.close()
     sleep(.1) # Delay for one tenth of a second
 
-fs_watcher = QtCore.QFileSystemWatcher(['fileToMonitor.txt'])
+
+fs_watcher = QtCore.QFileSystemWatcher([filepath])
 
 fs_watcher.connect(fs_watcher, QtCore.SIGNAL('fileChanged(QString)'), file_changed)
 
